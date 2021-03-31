b7f2ad8478d58bea6fca416f3e2cfaea75874738
nts-dev <daniel@nts.nl>
2021-03-31 17:31:40 +0300

    changed player to html5

--- play/index.php
+++ play/index.php
@@ -4,14 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
+
+    header("Access-Control-Allow-Origin: *");
+//    header('Access-Control-Allow-Credentials: true');
+//    header('Access-Control-Max-Age: 86400');    // cache for 1 day
 
 
+$ROOT = "http://". $_SERVER['HTTP_HOST']. "/nts-programs/nts-video";
 $id = $_GET['id'];
 
 include('../api/session/Commons.php');
@@ -37,28 +36,33 @@ $resultArray = $mediaService->findById($id);
 <head>
     <meta charset="utf-8">
     <title>Player</title>
-    <link href="../lib/video-js/video-js.css" rel="stylesheet">
-    <link href="../lib/videojs-share/video-share.css" rel="stylesheet">
+<!--    <link href="../lib/video-js/video-js.css" rel="stylesheet">-->
+<!--    <link href="../lib/videojs-share/video-share.css" rel="stylesheet">-->
     <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-
+<style>
+    html, body {height: 100%}
+    .video {
+        width: 100%;
+        height: 100%;
+    }
+</style>
 </head>
 
 <body>
-<!--<div class="container">-->
-<div class="row">
+<div class="container-fluid h-100">
+<div class="row justify-content-center h-100">
     <video
             id="my-video"
-            class="video-js"
+            class="video-js video"
             controls
             preload="auto"
             data-setup="{}">
-        <source src="<?php echo $resultArray->videoLink_raw; ?> " type="video/mp4">
+        <source src="<?php echo $resultArray->videoLink_raw; ?>" type="video/mp4">
 
     </video>
 </div>
-
-    <script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
+</div>
 
 </body>
 </html>
